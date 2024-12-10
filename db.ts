bfb158833e252d995ddfeefadd1d4f355cf935b4
yashkatore31 <ybkatore31@gmail.com>
2024-12-10 19:44:02 +0530

    created db schemas

--- db.ts
+++ db.ts
@@ -1,1 +1,11 @@
+import { Model } from 'mongoose';
 //create the schemas and user models for the db
+
+import { Schema,Model } from "mongoose";
+
+const UserSchema = new Schema({
+    username : {type: String , unique: true },
+    password : String
+})
+
+export const UserModel = new Model(UserSchema , "User");
