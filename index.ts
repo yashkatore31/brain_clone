f9c00d1a4fb7812365e6429d17ddedc57a123a58
yashkatore31 <ybkatore31@gmail.com>
2024-12-10 19:04:16 +0530

    created a file and addedd dependiesies

---
+++ index.ts
@@ -0,0 +1,42 @@
+import { Mongoose } from './../node_modules/mongoose/types/index.d';
+import express from "express";
+import { Mongoose } from "mongoose";
+import jwt from "jsonwebtoken";
+
+
+const app = express();
+
+//post
+app.post('/api/v1/signup',(req,res)=>{
+
+})
+
+app.post('/api/v1/sigin',(req,res)=>{
+    
+})
+
+app.post('/api/v1/content',(req,res)=>{
+    
+})
+
+//get
+app.get('/api/v1/content',(req,res)=>{
+    
+})
+
+
+//delet
+app.delet('/api/v1/content',(req,res)=>{
+    
+})
+
+
+//post
+app.post("/api/v1/share",(req,res) =>{
+
+})
+
+//get
+app.get("/api/v1/shareLink",(req,res) =>{
+    
+})
