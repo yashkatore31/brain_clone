8f8431a156357cd5a7234f409c8353317e474e2a
yashkatore31 <ybkatore31@gmail.com>
2024-12-11 20:32:15 +0530

    created get content and delet content

--- src/index.ts
+++ src/index.ts
@@ -73,10 +73,29 @@ app.post("/api/v1/content", UserMiddleware, async (req, res) => {
 });
 
 //get
-app.get("/api/v1/content", (req, res) => {});
+app.get("/api/v1/content", async(req, res) => {
+  const userId = req.userID;
+  const content = await ContentModel.find({
+    userId: userId
+  }).populate("userId","username")
+  res.json({
+    content
+  })
+});
 
 //delet
-app.delete("/api/v1/content", (req, res) => {});
+app.delete("/api/v1/content", (req, res) => {
+  const contentId = req.body.contentId;
+
+  await ContentModel.deleteMany({
+    content,
+    //@ts-ignore
+    userId: req.userId
+  })
+  res.json({
+    msg:"deleeted"
+  })
+});
 
 //post
 app.post("/api/v1/share", (req, res) => {});
